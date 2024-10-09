562af1be6f411921dc1e814a61a956dd0b54511d
Svyat-p <avtolegion25@gmail.com>
2024-10-10 00:10:59 +0300

    dfs

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.PrintStream;
 import java.util.Scanner;
-public class Main
-{
+public class Main {
+    public static Scanner in = new Scanner(System.in); // Объявляем объект класса Scanner для ввода данных
+    public static PrintStream out = System.out; // Объявляем объект класса PrintStream для вывода данных
     public static void main(String[] args) {
-        Scanner x=new Scanner(System.in);
-        int num=x.nextInt();
-        String a="";
-        while(num>=1){
-            a=a+num%2;
-            num=num/2;
+        int x = in.nextInt(); // Вводим объем контейнера
+        int a = in.nextInt(); // Вводим объем пакета А
+        int b = in.nextInt(); // Вводим объем пакета B
+        int c = in.nextInt(); // Вводим объем пакета C
+        int counter = 0; // Создаем счетчик, который будет считать количество пакетов, которые не поместились
+        if (x >= a) {
+            // Проверяем, помещается ли пакет А в контейнер.
+            // Если помещается, то вычитаем из переменной х(объем контейнера) объем пакета А и перезаписываем х, иначе прибавляем к счетчику единицу
+            x -= a;
+        }
+        else {
+            counter += 1;
+        }
+        if (x >= b) {
+            // Проверяем, помещается ли пакет В в оставшееся в контейнере место(х).
+            // Если помещается, то из переменной х вычитаем объем пакета В и перезаписываем х, иначе прибавляем к счетчику единицу
+            x -= b;
+        }
+        else {
+            counter +=1;
+        }
+        if (x < c) {
+            // Проверяем, больше ли объем пакета С, чем оставшееся в контейнере место(х)
+            // Если да, то прибавляем к счетчику единицу
+            counter += 1;
 
         }
-        String reverse="";
-        char b;
-        for(int i=a.length()-1; i>=0; i=i-1){
-            b=a.charAt(i);
-            reverse=reverse+b;
-
-        }
-        System.out.println(reverse);
-
-
+        out.print(counter); // Выводим итоговое количество пакетов, которые не поместились в контейнер
     }
 }
